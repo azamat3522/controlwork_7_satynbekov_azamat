063fccc9895a2c9ed8bfcec9d11b0c3901194a45
User <User@Users-MacBook-Pro.local>
2019-10-19 12:25:41 +0600

    Создал модель и применил миграцию

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    question = models.TextField(max_length=2000, null=False, blank=False, verbose_name='Вопрос')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+
+    def __str__(self):
+        return self.question
+
+
+class Choice(models.Model):
+    text = models.TextField(max_length=2000, null=False, blank=False, verbose_name='Текст варианта')
+    poll = models.ForeignKey('webapp.Poll', related_name='choice', on_delete=models.CASCADE, verbose_name='опрос')
+
+    def __str__(self):
+        return self.text
+
